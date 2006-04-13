60810f5e64eb97519a98249782453af4ecac6193
albertca <albertca@05b5a0de-b9f7-0310-a06d-afaf3a29ab14>
2006-04-13 15:37:30 +0000

    Add Document class to test QByteArray.

---
+++ trunk/test/document.h
@@ -0,0 +1,43 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Albert Cervera Areny                            *
+ *   albertca@hotpop.com                                                   *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef DOCUMENT_H
+#define DOCUMENT_H
+
+#include <object.h>
+
+/**
+@author Albert Cervera Areny
+*/
+class Document : public Object
+{
+	Q_OBJECT
+	Q_PROPERTY( QByteArray data WRITE setData READ data );
+
+public:
+	DCLASS( Document );
+
+	void setData( const QByteArray& data );
+	const QByteArray& data() const;
+
+private:
+	QByteArray m_data;
+};
+
+#endif
